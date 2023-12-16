5823f61f0dc410ec52ac83852be68a636f059fdb
Rougeetnoir <yangyaner1@gmail.com>
2023-12-15 16:22:45 -0800

    adding navigation function to menu button

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Highlights() {
+    const navigate = useNavigate();
+    const handleMenu = () => {
+        navigate('/menu');
+    };
     return (
         <div className='highlights-container'>
             <h1>This week's specials!</h1>
-            <button>Online Menu</button>
+            <button onClick={handleMenu}>Online Menu</button>
         </div>
     )
 }
